47ae1b383888afc3b07ba1cf8119deb7a7ab4f95
andisiambuku <roselandambuku@gmail.com>
2022-08-25 11:36:01 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData( name, email ) {
+    return fetch( 'http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify( {name,email} )
+      } )
+      // convert to json format
+      .then( function (response) {
+        return response.json()
+      } )
+      //to post the info needed
+      .then( function (object) {
+        document.body.innerHTML = object["id"]
+      } )
+      //to catch erros
+      .catch( function (error) {
+        document.body.innerHTML=error.message
+      } )
+  }
